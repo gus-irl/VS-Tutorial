623e3180717f12fea9926a43d3f267d16bb47c69
gus-irl <108893691+gus-irl@users.noreply.github.com>
2022-07-11 10:07:05 -0400

    update

---
+++ helloWorld.py
@@ -0,0 +1,18 @@
+# function a()
+from random import random
+
+
+print("AAAAA" * 100)
+
+
+
+msg = "Hello World"
+
+x = 2
+y = 5
+z = 2.3
+my_boolean = True
+
+print(type(424234 / 1248492 * 5 -2))
+print(round(3/2))
+print(random() * 100)
